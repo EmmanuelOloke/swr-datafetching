cde07af1d39b975097d091ae2faf35de13fd10dc
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-04-07 22:32:42 +0100

    swapped useEffect with useSWR

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import './App.css';
 
-import { ResponseInterface, ResultsEntity, Name } from './types';
-
-type User = ResponseInterface;
-type Results = ResultsEntity;
-type UserName = Name;
+import { ResultsProperties } from './types';
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  const apiUrl = 'https://randomuser.me/api/';
 
-  const fetchUser = async () => {
-    const response = await fetch('https://randomuser.me/api/');
+  const fetcher = async (url: string) => {
+    const response = await fetch(url);
     const data = await response.json();
-    setUser(data);
+    return data.results[0];
   };
 
-  useEffect(() => {
-    fetchUser();
-  }, []);
+  const { data: user, isLoading, error } = useSWR<ResultsProperties>(apiUrl, fetcher);
 
-  // const results = user.results<ResultsEntity>
-  // console.log(user?.results?.name);
+  const avatar = user?.picture;
+  const largeAvatar = avatar?.large;
+
+  const name = user?.name;
+  const fullName = `${name?.title} ${name?.first} ${name?.last}`;
 
   return (
     <div className="App">
-      <p>It happens here!</p>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : error ? (
+        <h3>An error occured</h3>
+      ) : (
+        <>
+          <img src={largeAvatar} alt="medium sized avatar image" style={{ borderRadius: '50%' }} />
+          <h3>{fullName}</h3>
+        </>
+      )}
     </div>
   );
 }
